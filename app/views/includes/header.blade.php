075462a6afe1cd67c2af92691ea6911d121c5b1f
Remy Gorissen <remy.gorissen@gmail.com>
2014-10-24 16:58:18 +0200

    header en footer omgewisseld, responsive menu en op main page moet nog gemaakt worden

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,9 +1,24 @@
 
 
-<div class="col-md-12 mainheader">
+<div class="col-sm-12 mainheader">
 
-    <div class="col-md-10">
-        
+    <div class="col-sm-2">
+        <div class="logoheader">
+            <a href="{{ URL::asset('/') }}"><img src="{{ URL::asset('images/logo.png') }}" /></a>
+        </div>
+    </div>
+
+
+    <div class="col-sm-5 menu">
+        <ul class='tabrow'>
+            <li class='selectedtab'><a href="{{url('/')}}">Home</a></li>
+            <li class="countrymenuitem"><a id="countrytab" href="#">Countries</a></li>
+            <li><a href="{{url('/random')}}">Random cols</a></li>
+            <li><a href="{{url('/help')}}">Help <i class="glyphicon glyphicon-question-sign"></i></a></li>
+            <li><a href="{{url('/about')}}">About <i class="glyphicon glyphicon-info-sign"></i></a></li>
+        </ul>
+    </div>
+    <div class="col-sm-5">
         <div class="limitsearchflag"></div>
         <form class="navbar-form" role="search">
             <div class="input-group add-on">
@@ -13,13 +28,8 @@
                 </div>
             </div>
         </form>
-        
     </div>
-    
-    <div class="col-md-2">
-        <div class="logoheader">
-            <a href="{{ URL::asset('/') }}"><img src="{{ URL::asset('images/logo.png') }}" /></a>
-        </div>
-    </div>    
+
+
 </div>
 
